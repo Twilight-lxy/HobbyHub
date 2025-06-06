481bf1ffc71e9f27d5f094d62ce157755c1d5fc2
zhangzhiguo <2997315060@qq.com>
2025-06-06 23:41:08 +0800

    test6

---
+++ management-web/api_user.js
@@ -0,0 +1,16 @@
+import request from '@/utils/request'
+
+export function getUserList(params) {
+  return request({
+    url: '/api/v1/admin/users',
+    method: 'get',
+    params
+  })
+}
+
+export function getUserDetail(id) {
+  return request({
+    url: `/api/v1/admin/users/${id}`,
+    method: 'get'
+  })
+}
